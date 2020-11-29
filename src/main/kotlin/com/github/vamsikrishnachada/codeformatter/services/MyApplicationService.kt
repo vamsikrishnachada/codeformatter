99888c0a00e0b191d2ce5a9140938a4cf9262314
GitHub Action <action@github.com>
2020-11-29 10:26:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vamsikrishnachada/codeformatter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vamsikrishnachada.codeformatter.services
+
+import com.github.vamsikrishnachada.codeformatter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
